1bc8bbcdf47814e62439eadadca9da7ba07a9870
Azizbek201798 <ziyodulayeva9@gmail.com>
2024-07-09 21:36:14 +0500

    O

---
+++ Forms/Homework/Task_7.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Convertor</title>
+</head>
+<body>
+
+    <?php $total = 0; ?>
+
+    <h1>Convert Bytes to Kilobytes to Megabytes to Gigabytes to Terabytes, and Vice Versa</h1>
+    <input type="integer">
+    <input type="integer">
+    <input type="integer">
+    <input type="integer">
+    <input type="integer"><br>
+    <label>Bytes</label>
+    <label>Kilobytes</label>
+    <label>Megabytes</label>
+    <label>Gigabytes</label>
+    <label>Terabytes</label>
+
+</body>
+</html>
---
+++ Forms/Homework/Task_8.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pitsa Buyurtma</title>
+</head>
+
+<body>
+
+    <h1>Pitsa buyurtma qilish</h1>
+    <h3>Noni qanday bo'lsin?</h3>
+    <select name="non" id="">
+        <option value="">Yupqa</option>
+        <option value="">Qalin</option>
+    </select>
+    <h3>Kattaligi</h3>
+    
+    <svg width="400" height="200">
+        <a href="#">
+            <circle cx="60" cy="60" r="50" fill="white" stroke="black" stroke-width="2" />
+            <text x="60" y="60" text-anchor="middle" alignment-baseline="middle" fill="black">25 sm</text>
+        </a>
+  
+        <a href="#">
+          <circle cx="190" cy="60" r="50" fill="white" stroke="black" stroke-width="2" />
+          <text x="190" y="60" text-anchor="middle" alignment-baseline="middle" fill="black">30 sm</text>
+        </a>
+  
+        <a href="#">
+          <circle cx="320" cy="60" r="50" fill="white" stroke="black" stroke-width="2" />
+          <text x="320" y="60" text-anchor="middle" alignment-baseline="middle" fill="black">35 sm</text>
+        </a>
+    </svg>
+    <h3>Ustiga nima solaylik?</h3>
+
+    <input type="checkbox" name="option1" value="option_1"> Pomidor
+    <input type="checkbox" name="option2" value="option_2"> Kurka go'shti
+    <input type="checkbox" name="option3" value="option_3"> Zaytun 
+    <input type="checkbox" name="option4" value="option_4"> Qazi
+    <input type="checkbox" name="option5" value="option_5"> Qo'ziqorin
+    <input type="checkbox" name="option6" value="option_6"> Tuzlangan bodring
+
+
+
+
+
+
+</body>
+
+</html>
