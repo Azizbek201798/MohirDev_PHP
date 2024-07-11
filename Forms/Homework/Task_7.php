cc4d935221f2b9a2e7330e1874b6e7fce8156b19
Azizbek201798 <ziyodulayeva9@gmail.com>
2024-07-12 04:12:00 +0500

    One

--- Forms/Homework/Task_7.php
+++ Forms/Homework/Task_7.php
@@ -3,23 +3,93 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Convertor</title>
+    <title>Converter</title>
+    <style>
+        body {
+            font-family: "Times New Roman", sans-serif;
+            margin: 0;
+            padding: 20px;
+            background-color: #f1f1f1;
+        }
+
+        h1 {
+            text-align: center;
+            color: #333;
+        }
+
+        .input-container {
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: center;
+            align-items: flex-start;
+            margin-bottom: 10px;
+        }
+
+        .input-container label {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            width: 120px;
+            margin-right: 10px;
+            font-weight: bold;
+        }
+
+        .input-container input[type="number"] {
+            width: 100px;
+            padding: 10px;
+        }
+    </style>
 </head>
 <body>
+    <?php
+    function calculateValues($inputName, $inputValue) {
+        if ($inputValue !== 0) {
+            $byteValue = $inputName === 'byte' ? $inputValue : 0;
+            $kbyteValue = $inputName === 'kbyte' ? $byteValue / 1024 : $byteValue / 1024;
+            $megabyteValue = $inputName === 'megabyte' ? $kbyteValue / 1024 : $kbyteValue / 1024;
+            $gigabyteValue = $inputName === 'gigabyte' ? $megabyteValue / 1024 : $megabyteValue / 1024;
+            $terabyteValue = $inputName === 'terabyte' ? $megabyteValue : $gigabyteValue / 1024;
 
-    <?php $total = 0; ?>
+            return array(
+                'byte' => $byteValue,
+                'kbyte' => $kbyteValue,
+                'megabyte' => $megabyteValue,
+                'gigabyte' => $gigabyteValue,
+                'terabyte' => $terabyteValue
+            );
+        } else {
+            return array(
+                'byte' => '',
+                'kbyte' => '',
+                'megabyte' => '',
+                'gigabyte' => '',
+                'terabyte' => ''
+            );
+        }
+    }
 
-    <h1>Convert Bytes to Kilobytes to Megabytes to Gigabytes to Terabytes, and Vice Versa</h1>
-    <input type="integer">
-    <input type="integer">
-    <input type="integer">
-    <input type="integer">
-    <input type="integer"><br>
-    <label>Bytes</label>
-    <label>Kilobytes</label>
-    <label>Megabytes</label>
-    <label>Gigabytes</label>
-    <label>Terabytes</label>
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $byteValue = isset($_POST['byte']) ? (int)$_POST['byte'] : 0;
+
+        $calculatedValues = calculateValues('byte', $byteValue);
+    } else {
+        $calculatedValues = calculateValues('byte', 0);
+    }
+    ?>
+
+    <h1>Convert Bytes to Kilobytes to Megabytes to Gigabytes to Terabytes</h1>
+
+    <form method="POST">
+        <div class="input-container">
+            <label>Bytes<input type="number" name="byte" value="<?php echo $calculatedValues['byte']; ?>" oninput="this.form.submit()" /></label>
+            <label>Kilobytes<input type="number" name="kbyte" value="<?php echo $calculatedValues['kbyte']; ?>" readonly /></label>
+            <label>Megabytes<input type="number" name="megabyte" value="<?php echo $calculatedValues['megabyte']; ?>" readonly /></label>
+            <label>Gigabytes<input type="number" name="gigabyte" value="<?php echo $calculatedValues['gigabyte']; ?>" readonly /></label>
+            <label>Terabytes<input type="number" name="terabyte" value="<?php echo $calculatedValues['terabyte']; ?>" readonly /></label>
+        </div>
+
+        <noscript><input type="submit" value="Calculate"></noscript>
+    </form>
 
 </body>
 </html>
--- Forms/Homework/Task_8.php
+++ Forms/Homework/Task_8.php
@@ -5,47 +5,110 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pitsa Buyurtma</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <style>
+
+        .vertical-line {
+            position: fixed;
+            top: 0;
+            bottom: 0;
+            width: 2px;
+            background-color: black;
+            left: calc(35% + 10px);
+        }
+        
+        .label_1 {
+            position: fixed;
+            top: 20px;
+            left: calc(35% + 50px);
+            font-size: 20px;
+        }
+        
+        .label_2 {
+            position: fixed;
+            top: 80px;
+            left: calc(35% + 50px);
+            font-size: 20px;
+        }
+        .label_3 {
+            position: fixed;
+            top: 140px;
+            left: calc(35% + 50px);
+            font-size: 20px;
+        }
+        .label_4 {
+            position: fixed;
+            top: 280px;
+            left: calc(35% + 50px);
+            font-size: 20px;
+        }
+        .button-container {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            height: 100vh; 
+        }
+
+        .button-container .btn {
+            margin-top: auto; 
+        }
+        
+    </style>
 </head>
 
 <body>
-
-    <h1>Pitsa buyurtma qilish</h1>
+    <h1>Pitsa buyurtma qilish</h1><br>
     <h3>Noni qanday bo'lsin?</h3>
-    <select name="non" id="">
-        <option value="">Yupqa</option>
-        <option value="">Qalin</option>
+    <select >
+        <option value="Yupqa" name="option" >Yupqa</option>
+        <option value="Qalin" name="option" >Qalin</option>
     </select>
-    <h3>Kattaligi</h3>
-    
+    <h3>Kattaligi</h3><br>
+
     <svg width="400" height="200">
         <a href="#">
             <circle cx="60" cy="60" r="50" fill="white" stroke="black" stroke-width="2" />
             <text x="60" y="60" text-anchor="middle" alignment-baseline="middle" fill="black">25 sm</text>
         </a>
-  
+
         <a href="#">
           <circle cx="190" cy="60" r="50" fill="white" stroke="black" stroke-width="2" />
           <text x="190" y="60" text-anchor="middle" alignment-baseline="middle" fill="black">30 sm</text>
         </a>
-  
+
         <a href="#">
           <circle cx="320" cy="60" r="50" fill="white" stroke="black" stroke-width="2" />
           <text x="320" y="60" text-anchor="middle" alignment-baseline="middle" fill="black">35 sm</text>
         </a>
     </svg>
+    
     <h3>Ustiga nima solaylik?</h3>
+        <input type="checkbox" name="ustiga[]" value="Pomidor" id="ustiga_pomidor">
+        <label for="ustiga_pomidor">Pomidor</label>
+        <input type="checkbox" name="ustiga[]" value="Qazi" id="ustiga_qazi">
+        <label for="ustiga_qazi">Qazi</label>
+        <input type="checkbox" name="ustiga[]" value="Qo'ziqorin" id="ustiga_qoziqorin">
+        <label for="ustiga_qoziqorin">Qo'ziqorin</label>
+        <input type="checkbox" name="ustiga[]" value="Zaytun" id="ustiga_zaytun">
+        <label for="ustiga_zaytun">Zaytun</label>
 
-    <input type="checkbox" name="option1" value="option_1"> Pomidor
-    <input type="checkbox" name="option2" value="option_2"> Kurka go'shti
-    <input type="checkbox" name="option3" value="option_3"> Zaytun 
-    <input type="checkbox" name="option4" value="option_4"> Qazi
-    <input type="checkbox" name="option5" value="option_5"> Qo'ziqorin
-    <input type="checkbox" name="option6" value="option_6"> Tuzlangan bodring
+        <h3>Qo'shimchalar</h3>
+        <input type="checkbox" name="qoshimcha[]" value="Achchiq" id="qoshimcha_achchiq">
+        <label for="qoshimcha_achchiq">Achchiq</label>
+        <input type="checkbox" name="qoshimcha[]" value="Sosiskali" id="qoshimcha_sosiskali">
+        <label for="qoshimcha_sosiskali">Sosiskali</label>
 
+    <h2>Chopar pitsa maxsus buyurmasi!</h2>
 
+    <div class="vertical-line"></div>
 
+    <label class="label_1">Non: <?php ?> </label>
+    <label class="label_2">Kattaligi: <?php echo $_POST['option'];?> </label>
+    <label class="label_3">Ustida: <?php echo $_POST['option'];?> </label>
+    <label class="label_4">Qo'shimcha: <?php echo $_POST['option'];?> </label>
 
-
+    <button type="button" class="btn btn-success">Buyurtma berish</button>
 
 </body>
 
